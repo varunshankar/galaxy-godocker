75138bbf1cdbf999a309beae944d34e7f85c0e87
Varun Shankar S <varunshankar55@gmail.com>
2016-05-29 13:29:12 +0530

    indentation change when git push

--- bin/godocker.py
+++ bin/godocker.py
@@ -13,45 +13,42 @@ log = logging.getLogger(__name__)
 __all__ = ['GodockerJobRunner']
 
 class GodockerJobRunner(AsynchronousJobRunner):
-	"""
+    """
 	Job runner backed by a finite pool of worker threads. FIFO scheduling
     """
     runner_name = "GodockerJobRunner"
     
-	def __init__(self, app, nworkers, **kwargs):
-		log.debug("Loading app %s",app)
-	
-		runner_param_specs = dict(
-			godocker_master = dict(map = str,default = os.environ.get('godocker_master', None)),
-			job_name = dict(map = str),
-			cpu = dict(map = int,valid = lambda x: int > 0,default = 1),
-			ram = dict(map = int,valid = lambda x: int > 0,default = 1),
-			image = dict(map = str,valid = lambda x: x in ("centos:latest","rastasheep/ubuntu-sshd","osallou/dockernode")),
-			user = dict(map = str),
-			key = dict(map = str)
+    def __init__(self, app, nworkers, **kwargs):
+    	log.debug("Loading app %s",app)
+    	
+    	runner_param_specs = dict(
+    		godocker_master = dict(map = str),
+    		job_name = dict(map = str),
+    		cpu = dict(map = int,valid = lambda x: int > 0,default = 1),
+    		ram = dict(map = int,valid = lambda x: int > 0,default = 1),
+    		image = dict(map = str), #"centos:latest","rastasheep/ubuntu-sshd","osallou/dockernode"
+    		user = dict(map = str),
+    		key = dict(map = str)
         )
         if 'runner_param_specs' not in kwargs:
-			kwargs['runner_param_specs'] = dict()
-
-		kwargs['runner_param_specs'].update(runner_param_specs)
-
-		# godocker API login call to be done here
-
-		super(GodockerJobRunner, self).__init__(app, nworkers, **kwargs)
-		""" Following methods starts threads.
-			threading.Thread(name,target) invokes methods monitor() and run_next() 
-		"""
-		self._init_monitor_thread()
+        	kwargs['runner_param_specs'] = dict()
+        
+        kwargs['runner_param_specs'].update(runner_param_specs)
+        
+        # godocker API login call to be done here
+        
+        super(GodockerJobRunner, self).__init__(app, nworkers, **kwargs)
+        """ Following methods starts threads.
+            threading.Thread(name,target) invokes methods monitor() and run_next()
+        """
+        self._init_monitor_thread()
         self._init_worker_threads()
 
     def queue_job(self, job_wrapper):
-
-        log.debug("Starting queue_job for job " + job_wrapper.get_id_tag())
+    	log.debug("Starting queue_job for job " + job_wrapper.get_id_tag())
 		
-		
-
-	def stop_job(self,job):
-		pass
-
-	def recover(self,job):
-		pass
+    def stop_job(self,job):
+    	pass
+    
+    def recover(self,job):
+    	pass
