a5f9333d34cdfd5f9b5fb1a5851ecc441d03c86a
varunshankar <varunshankar55@gmail.com>
2016-06-19 00:41:55 +0530

    Re-write godocker auth, httputils

--- bin/godocker.py
+++ bin/godocker.py
@@ -3,18 +3,132 @@ import os
 import json
 import time
 import inspect
+import requests
 from datetime import datetime
 
 from galaxy import model
 from galaxy.jobs.runners import AsynchronousJobState, AsynchronousJobRunner
 
-from godockercli.auth import Auth
-from godockercli.httputils import HttpUtils
-from godockercli.utils import Utils
 
 log = logging.getLogger(__name__)
 
 __all__ = ['GodockerJobRunner']
+
+class Godocker(object):
+    """
+    API parameters 
+    """
+    def __init__(self, server, login, apikey,noCert):
+        self.token = None
+        self.server = server
+        self.login = login
+        self.apikey = apikey
+        self.noCert = noCert
+
+    def setToken(self, token):
+        self.token = token
+
+    def http_post_request(self, query, data, header):
+        """ post request with query """
+
+        #remove warnings if using --no-certificate
+        requests.packages.urllib3.disable_warnings()
+        verify_ssl = not self.noCert
+        try:
+            url= self.server+query
+            res = requests.post(url, data, headers=header, verify=verify_ssl)
+            print('Godocker Response:',res, "\n")
+
+        except requests.exceptions.ConnectionError as e:
+            print('A Connection error occurred:', e)
+            if re.search("SSL3_GET_SERVER_CERTIFICATE", str(e)):
+                print("Use the --no-certificate option if you trust the remote godocker server certificate.")
+            return False
+
+        except requests.exceptions.HTTPError as e:
+            print('A HTTP error occurred:', e)
+            return False
+
+        return self.test_status_code(res)
+        
+
+    def http_get_request(self, query, header):
+        """ get request with query, server and header required """
+
+        #remove warnings if using --no-certificate
+        requests.packages.urllib3.disable_warnings()
+        verify_ssl = not self.noCert
+        try:
+            url= self.server+query
+            res = requests.get(url, headers=header, verify=verify_ssl)
+
+        except requests.exceptions.ConnectionError as e:
+            print('A Connection error occurred:', e)
+            return False
+
+        except requests.exceptions.HTTPError as e:
+            print('A HTTP error occurred:', e)
+            return False
+
+        return self.test_status_code(res)
+
+    def http_delete_request(self, query, header):
+        """ delete request with query, server and header required """
+
+        #remove warnings if using --no-certificate
+        requests.packages.urllib3.disable_warnings()
+        verify_ssl = not self.noCert
+        try:
+            url= self.server+query
+            res = requests.delete(url, headers=header, verify=verify_ssl)
+
+        except requests.exceptions.ConnectionError as e:
+            print('A Connection error occurred:', e)
+            return False
+
+        except requests.exceptions.HTTPError as e:
+            print('A HTTP error occurred:', e)
+            return False
+
+        return self.test_status_code(res)
+
+    def http_put_request(self, query, data, header):
+        """ put request with query """
+
+        #remove warnings if using --no-certificate
+        requests.packages.urllib3.disable_warnings()
+        verify_ssl = not self.noCert
+        try:
+            url= self.server+query
+            res = requests.put(url, data, headers=header, verify=verify_ssl)
+
+        except requests.exceptions.ConnectionError as e:
+            print('A Connection error occurred:', e)
+            return False
+
+        except requests.exceptions.HTTPError as e:
+            print('A HTTP error occurred:', e)
+            return False
+
+        return self.test_status_code(res)
+        
+
+    def test_status_code(self,httpresult):
+        """ exit if status code is 401 or 403 or 404 or 200"""
+        if httpresult.status_code == 401:
+            print('Unauthorized : this server could not verify that you are authorized to access the document you requested.')
+
+        elif httpresult.status_code == 403:
+            print('Forbidden : Access was denied to this resource. Not authorized to access this resource.')
+
+        elif httpresult.status_code == 404:
+            print('Not Found : The resource could not be found.')
+
+        elif httpresult.status_code == 200:
+            return httpresult
+
+        return False
+
 
 class GodockerJobRunner(AsynchronousJobRunner):
     """
@@ -25,13 +139,12 @@ class GodockerJobRunner(AsynchronousJobRunner):
     def __init__(self, app, nworkers, **kwargs):
 
     	log.debug("Loading app %s",app)
-    	self.auth = None
         #self.server = "https://godocker.genouest.org" #"https://docker-ui/genouest.org"
     	runner_param_specs = dict(
     		godocker_master = dict(map = str),
     		user = dict(map = str),
     		key = dict(map = str),
-            galaxy_master = dict(map = str)
+            godocker_project = dict(map = str)
         )
         if 'runner_param_specs' not in kwargs:
         	kwargs['runner_param_specs'] = dict()
@@ -220,53 +333,53 @@ class GodockerJobRunner(AsynchronousJobRunner):
         log.debug("LOGIN TASK TO BE EXECUTED \n")
         log.warn("GODOCKER LOGIN: "+str(login)+":"+str(apikey))
         data=json.dumps({'user': login, 'apikey': apikey})
-        auth = HttpUtils.http_post_request("/api/1.0/authenticate",data,server,{'Content-type': 'application/json','Accept': 'application/json'},"False")
+        g_auth = Godocker(server,login,apikey,noCert)
+        auth = g_auth.http_post_request("/api/1.0/authenticate",data,{'Content-type': 'application/json','Accept': 'application/json'})
         self.get_structure(auth)
-        
         if not auth:
             print("Authentication Error!!")
         else:
-            Auth.create_auth_file(apikey, login, server, noCert)
-            auth = auth.json()['token']
-            log.debug(auth)
+            token = auth.json()['token']
+            g_auth.setToken(token)
+            log.debug(token)
         log.debug("END OF LOGIN TASK \n")
-        return auth
+        return g_auth
 
 
     def post_task(self,job_wrapper):
         #Sumbit job to godocker
-        Auth.authenticate()
-        log.debug("\n INSIDE JOB CREATION TEMPLATE \n")
-        job_destination = job_wrapper.job_destination
-        docker_repo = job_destination.params["docker_repo_override"]
-        docker_owner = job_destination.params["docker_owner_override"]
-        docker_image = job_destination.params["docker_default_container_id"]
-        docker_tags = job_destination.params["docker_tag_override"]
-        docker_cpu = job_destination.params["docker_cpu"]
-        docker_ram = job_destination.params["docker_memory"]
+        #Auth.authenticate()
+        if self.auth.token:
+            log.debug("\n INSIDE JOB CREATION TEMPLATE \n")
+            job_destination = job_wrapper.job_destination
+            docker_repo = job_destination.params["docker_repo_override"]
+            docker_owner = job_destination.params["docker_owner_override"]
+            docker_image = job_destination.params["docker_default_container_id"]
+            docker_tags = job_destination.params["docker_tag_override"]
+            docker_cpu = job_destination.params["docker_cpu"]
+            docker_ram = job_destination.params["docker_memory"]
 
         
-        #volume = "home"
-        #docker_image="centos:latest"
-        volumes=[]
-        labels=[]
-        #tags
-        #tags_tab = docker_tags.split(",")
-        tags_tab = ['galaxy',job_wrapper.tool.id]
+            #volume = "home"
+            #docker_image="centos:latest"
+            volumes=[]
+            labels=[]
+            #tags
+            #tags_tab = docker_tags.split(",")
+            tags_tab = ['galaxy',job_wrapper.tool.id]
 
-        # manage depends
-        tasks_depends = []
-        name = job_wrapper.tool.name
-        description= "example job"
-        array = None
-        log.debug(self.runner_params["galaxy_master"])
-        project = "galaxy" #str(self.runner_params["galaxy_master"])
-        dt = datetime.now()
-        command = "#!/bin/bash\n"+"cd "+job_wrapper.working_directory+"\n"+job_wrapper.runner_command_line
-        log.debug("\n Command: ")
-        log.debug(command)
-
-        job = {
+            # manage depends
+            tasks_depends = []
+            name = job_wrapper.tool.name
+            description= "example job"
+            array = None
+            log.debug(self.runner_params["godocker_project"])
+            project = str(self.runner_params["godocker_project"])
+            dt = datetime.now()
+            command = "#!/bin/bash\n"+"cd "+job_wrapper.working_directory+"\n"+job_wrapper.runner_command_line
+            log.debug("\n Command: ")
+            log.debug(command)
+            job = {
         'user' : {
             #'id' : user_infos['id'],
             #'uid' : user_infos['uid'],
@@ -305,61 +418,59 @@ class GodockerJobRunner(AsynchronousJobRunner):
             'primary': None,
             'secondary': None
             }
-        }
-        log.debug("JOB TEMPLATE: ")
-        log.debug(job)
-        log.debug("END OF JOB TEMPLATE \n")
-        log.debug("\n JOB POST TASK TO BE EXECUTED \n")
-        result = HttpUtils.http_post_request(
-           "/api/1.0/task", json.dumps(job),
-           Auth.server,
-           {'Authorization':'Bearer '+Auth.token, 'Content-type': 'application/json', 'Accept':'application/json'},
-           Auth.noCert
-        )
-        #self.get_structure(result)
-        log.debug(result.text)
-        log.debug("Response from godocker: "+ str(result.json()['msg']) + " ID: " + str(result.json()['id']))
-        log.debug("END OF JOB POST TASK\n")
-        return str(result.json()['id'])
+            }
+            log.debug("JOB TEMPLATE: ")
+            log.debug(job)
+            log.debug("END OF JOB TEMPLATE \n")
+            log.debug("\n JOB POST TASK TO BE EXECUTED \n")
+            result = self.auth.http_post_request(
+                "/api/1.0/task", json.dumps(job),
+                {'Authorization':'Bearer '+self.auth.token, 'Content-type': 'application/json', 'Accept':'application/json'}
+            )
+            #self.get_structure(result)
+            log.debug(result.text)
+            log.debug("Response from godocker: "+ str(result.json()['msg']) + " ID: " + str(result.json()['id']))
+            log.debug("END OF JOB POST TASK\n")
+            return str(result.json()['id'])
 
     def get_task(self,job_id):
         #Get job details
         job = False
-        t = {"server":Auth.server,"noCert":Auth.noCert,"token":Auth.token,"login":Auth.login,"apikey":Auth.apikey}
+        t = {"server":self.auth.server,"noCert":self.auth.noCert,"token":self.auth.token,"login":self.auth.login,"apikey":self.auth.apikey}
         log.debug(t)
-        if Auth.token:
-            result = HttpUtils.http_get_request("/api/1.0/task/"+str(job_id),Auth.server,{'Authorization':'Bearer '+Auth.token},Auth.noCert)
+        if self.auth.token:
+            result = self.auth.http_get_request("/api/1.0/task/"+str(job_id),{'Authorization':'Bearer '+self.auth.token})
             job = result.json()
         return job
 
     def task_suspend(self,job_id):
         #Suspend actively running job
         job = False
-        if Auth.authenticate():
-            result = HttpUtils.http_get_request("/api/1.0/task/"+str(job_id)+"/suspend",Auth.server,{'Authorization':'Bearer '+Auth.token},Auth.noCert)
+        if self.auth.token:
+            result = self.auth.http_get_request("/api/1.0/task/"+str(job_id)+"/suspend",{'Authorization':'Bearer '+self.auth.token})
             job = result.json()
         return job
 
     def get_task_status(self,job_id):
         #Get job status
         job = False
-        t = {"server":Auth.server,"noCert":Auth.noCert,"token":Auth.token,"login":Auth.login,"apikey":Auth.apikey}
+        t = {"server":self.auth.server,"noCert":self.auth.noCert,"token":self.auth.token,"login":self.auth.login,"apikey":self.auth.apikey}
         log.debug(t)
-        if Auth.token:
+        if self.auth.token:
             log.debug("Authentication in process!!!")
-            result = HttpUtils.http_get_request("/api/1.0/task/"+str(job_id)+"/status",Auth.server,{'Authorization':'Bearer '+Auth.token},Auth.noCert)
+            result = self.auth.http_get_request("/api/1.0/task/"+str(job_id)+"/status",{'Authorization':'Bearer '+self.auth.token})
             job = result.json()
-        t = {"server":Auth.server,"noCert":Auth.noCert,"token":Auth.token,"login":Auth.login,"apikey":Auth.apikey}
+        t = {"server":self.auth.server,"noCert":self.auth.noCert,"token":self.auth.token,"login":self.auth.login,"apikey":self.auth.apikey}
         log.debug(t)
         return job
 
     def delete_task(self,job_id):
         #Delete a job 
         job = False
-        t = {"server":Auth.server,"noCert":Auth.noCert,"token":Auth.token,"login":Auth.login,"apikey":Auth.apikey}
+        t ={"server":self.auth.server,"noCert":self.auth.noCert,"token":self.auth.token,"login":self.auth.login,"apikey":self.auth.apikey}
         log.debug(t)
-        if Auth.token:
-            result = HttpUtils.http_delete_request("/api/1.0/task/"+str(job_id),Auth.server,{'Authorization':'Bearer '+Auth.token},Auth.noCert)
+        if self.auth.token:
+            result = self.auth.http_delete_request("/api/1.0/task/"+str(job_id),{'Authorization':'Bearer '+self.auth.token})
             job = result.json()
         return job
 
@@ -370,10 +481,4 @@ class GodockerJobRunner(AsynchronousJobRunner):
             log.debug(i)
         log.debug("\n END OF STRUCTURE \n")
         return
-    
-    def get_all_members_recursive(self,obj):
-        print inspect.getmembers(obj)
-        for i in inspect.getmembers(obj):
-            if "object" in str(i[1]) or "instance" in str(i[1]):
-                self.get_all_members_recursive(i[1])
 
