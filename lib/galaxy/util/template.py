8e863393eb564919c492cb992cb5e754e8c04650
varunshankar <varunshankar55@gmail.com>
2016-07-23 16:55:44 +0530

    Integrate with galaxy

---
+++ lib/galaxy/util/template.py
@@ -0,0 +1,7 @@
+from Cheetah.Template import Template
+
+
+def fill_template( template_text, context=None, **kwargs ):
+    if not context:
+        context = kwargs
+    return str( Template( source=template_text, searchList=[context] ) )
---
+++ lib/galaxy/web/__init__.py
@@ -0,0 +1,30 @@
+"""
+The Galaxy web application framework
+"""
+
+# flake8: noqa
+
+from framework import url_for
+from framework.decorators import error
+from framework.decorators import expose
+from framework.decorators import json
+from framework.decorators import json_pretty
+from framework.decorators import require_login
+from framework.decorators import require_admin
+from framework.decorators import expose_api
+from framework.decorators import expose_api_anonymous
+from framework.decorators import expose_api_raw
+from framework.decorators import expose_api_raw_anonymous
+
+# TODO: Drop and make these the default.
+from framework.decorators import _future_expose_api
+from framework.decorators import _future_expose_api_anonymous
+from framework.decorators import _future_expose_api_raw
+from framework.decorators import _future_expose_api_raw_anonymous
+from framework.decorators import _future_expose_api_anonymous_and_sessionless
+from framework.decorators import _future_expose_api_raw_anonymous_and_sessionless
+
+from framework.formbuilder import form
+from framework.formbuilder import FormBuilder
+
+from framework.base import httpexceptions
